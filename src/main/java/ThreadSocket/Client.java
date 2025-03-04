8b4129855578ccbaddeedefdd2415a8b07a08882
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2025-03-04 15:26:09 +0530

    socket mesage

--- src/main/java/ThreadSocket/Client.java
+++ src/main/java/ThreadSocket/Client.java
@@ -1,6 +1,9 @@
-package TCPSocket;
+package ThreadSocket;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
